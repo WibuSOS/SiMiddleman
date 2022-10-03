0e804db31a2abe3b4b2d26a0a8988332abdf74ac
Khalil Attalla Firdia <khalilattalla2209@gmail.com>
2022-10-03 15:34:44 +0700

    change model user

--- backend/models/users.go
+++ backend/models/users.go
@@ -4,6 +4,6 @@ import "gorm.io/gorm"
 
 type Users struct {
 	gorm.Model
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `json:"email" gorm:"unique;not null;type:varchar(255)"`
+	Password string `json:"password" gorm:"not null;type:varchar(255)"`
 }
