6ceb00ca34ec3c8f58e01b1baf8d05d15c246132
Khalil Attalla Firdia <khalilattalla2209@gmail.com>
2022-10-03 17:14:58 +0700

    try connect frontend backend login

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -6,9 +6,30 @@ import logo from './assets/logo.png'
 
 function Example() {
   const [show, setShow] = useState(false);
+  const [error, setError] = useState(null)
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const handlLogin = async (e) => {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const body = {
+      email: formData.get("email"),
+      password: formData.get("password")
+    }
+
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/login`, {
+        method: 'POST',
+        body: JSON.stringify(body)
+      })
+      const data = await res.json()
+      console.log(data);
+
+    } catch (error) {
+      setError(error)
+    }
+  }
 
   console.log(logo.src)
 
@@ -17,21 +38,24 @@ function Example() {
       <Button variant="primary" onClick={handleShow}>
         Login
       </Button>
+
+      {/* Form Login */}
       <Modal show={show} onHide={handleClose}
-      aria-labelledby="contained-modal-title-vcenter"
-      centered>
-        <Modal.Header closeButton>	
+        aria-labelledby="contained-modal-title-vcenter"
+        centered>
+        <Modal.Header closeButton>
           <div className="avatar">
-					  <img src={logo.src} alt="logo SiMiddleman+"/>
+            <img src={logo.src} alt="logo SiMiddleman+" />
           </div>
           <Modal.Title className="ms-auto">Login</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+          <Form onSubmit={handlLogin}>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Control
                 type="email"
                 placeholder="Email"
+                name="email"
                 autoFocus
               />
             </Form.Group>
@@ -39,14 +63,15 @@ function Example() {
               <Form.Control
                 type="password"
                 placeholder="Password"
+                name="password"
                 autoFocus
               />
             </Form.Group>
+            <div className='d-flex justify-content-between'>
+              <a>Lupa Password?</a>
+              <Button variant='merah' type='submit'>Masuk</Button>
+            </div>
           </Form>
-          <div className='d-flex justify-content-between'>
-            <a href="javascript:alert('not yet implemented')">Lupa Password?</a>
-            <Button variant='merah' onClick={handleClose}>Masuk</Button>
-          </div>
           <p className='or'>OR</p>
           <Button variant='merah' onClick={handleClose} className='w-100'>Daftar Akun</Button>
         </Modal.Body>
