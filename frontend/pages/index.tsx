a90e65fe541edc74ac495acd9554f1b918733335
juls <julyusmanurung@gmail.com>
2022-10-05 10:39:49 +0700

    refactor register as component
    + add sweet alert 2 for user feedback

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,86 +1,29 @@
-import Head from 'next/head';
-import { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
-import logo from './assets/logo.png'
-import LoginForm from './Login';
-import RegisterForm from './register';
-import { useRouter } from 'next/router';
 import { signOut, signIn, useSession } from "next-auth/react";
 import CreateRoom from './CreateRoom';
+import RegisterForm from './Register';
 
-function Home() {
-    const [show, setShow] = useState(false);
-    const [registerModal, setRegisterModal] = useState(false);
-    const openRegisterModal = () => setRegisterModal(true)
-    const closeRegisterModal = () => setRegisterModal(false)
+function Home() {    
+  const { data: session } = useSession();
 
-    const getData = async () => {
-        
-    }
-
-    const handleSubmitRegister = async (e) => {
-        e.preventDefault();
-        const formData = new FormData(e.currentTarget);
-    
-        const body = {
-            nama: formData.get("nama"),
-            noHp: formData.get("noHp"),
-            noRek: formData.get("noRek"),
-            email: formData.get("email"),
-            password: formData.get("password"),
-        }
-    
-        try {
-            const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/register`, {
-                method: 'POST',
-                body: JSON.stringify(body)
-            });
-            const data = await res.json();
-    
-            console.log(data)
-            if (data.message === "success"){
-                router.push('/')
-            }else {
-                alert("Register Gagal!")
-            }
-        }
-        catch (error) {
-            console.log(error);
-        }
-      }
-      
-      const router = useRouter();
-      const { data: session } = useSession();
-
-      console.log("session", session);
-
-      if (session) {
-        return (
-          <>
-            <Button onClick={() => signOut()}>Sign out</Button>
-            <CreateRoom/>
-          </>
-        )
-      }
-      else {
-        return (
-          <div className='container mx-10 my-7'>
-              <Button variant="primary" onClick={() => signIn()}>
-                  Login
-              </Button>
-  
-              <Button variant="primary" onClick={openRegisterModal}>
-                  Register
-              </Button>
-              
-              <RegisterForm handleSubmit={handleSubmitRegister}
-                  closeRegisterModal={closeRegisterModal}
-                  registerModal={registerModal}/>
-          </div>
-        );
-      }
+  if (session) {
+    return (
+      <>
+        <Button onClick={() => signOut()}>Sign out</Button>
+        <CreateRoom/>
+      </>
+    )
+  }
+  else {
+    return (
+      <div className='container mx-10 my-7'>
+          <Button variant="primary" onClick={() => signIn()}>
+              Login
+          </Button>
+          <RegisterForm/>
+      </div>
+    );
+  }
 }
 
 export default Home;
--- frontend/pages/register.tsx
+++ frontend/pages/register.tsx
@@ -1,77 +1,130 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Modal from 'react-bootstrap/Modal';
-import logo from './assets/logo.png'
+import { Form, Modal, Button } from 'react-bootstrap';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import logo from './assets/logo.png';
+import Swal from 'sweetalert2';
+import { signIn } from "next-auth/react";
 
-export default function RegisterForm({ handleSubmit, closeRegisterModal, registerModal }) {
-    return (       
-        <Modal show={registerModal} onHide={closeRegisterModal}
-            aria-labelledby="contained-modal-title-vcenter"
-            centered>
+export default function RegisterForm() {
+  const [registerModal, setRegisterModal] = useState(false);
+  const openRegisterModal = () => setRegisterModal(true);
+  const closeRegisterModal = () => setRegisterModal(false);
+  const router = useRouter();
 
-            <Modal.Header closeButton>	
-                <div className="avatar">
-                    <img src={logo.src} alt="logo SiMiddleman+"/>
-                </div>
-                <Modal.Title className="ms-auto">Register</Modal.Title>
-            </Modal.Header>
+  const handleSubmitRegister = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
 
-            <Modal.Body>
-                <Form onSubmit={handleSubmit}>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Control
-                            type="text"
-                            placeholder="Nama"
-                            name='nama'
-                            autoFocus
-                            required
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
-                        <Form.Control
-                            type="text"
-                            placeholder="No HP"
-                            name='noHp'
-                            required
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
-                        <Form.Control
-                            type="text"
-                            placeholder="No Rekening"
-                            name='noRek'
-                            required
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput4">
-                        <Form.Control
-                            type="email"
-                            placeholder="Email"
-                            name='email'
-                            required
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput5">
-                        <Form.Control
-                            type="password"
-                            placeholder="Password"
-                            name='password'
-                            required
-                            minLength={8}
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput6">
-                        <Form.Control
-                            type="password"
-                            placeholder="Confirm Password"
-                            name='confirmPassword'
-                            required
-                            minLength={8}
-                        />
-                    </Form.Group>
-                    <Button variant='merah' type='submit'>Submit</Button>
-                </Form>
-            </Modal.Body>
-        </Modal> 
-    )
+    const body = {
+      nama: formData.get("nama"),
+      noHp: formData.get("noHp"),
+      noRek: formData.get("noRek"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+    }
+
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/register`, {
+        method: 'POST',
+        body: JSON.stringify(body)
+      });
+      const data = await res.json();
+      
+      if (data.message === "success"){
+        Swal.fire({
+          icon: 'success',
+          title: 'Register berhasil, silahkan login',
+          confirmButtonText: 'Login',
+        }).then((result) => {
+          if (result.isConfirmed) {
+            signIn()
+          }
+        })
+      }else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Register gagal',
+          text: 'Terjadi kesalahan pada input anda',
+        })
+      }
+    }
+    catch (error) {
+        console.log(error);
+    }
+  }
+
+  return (
+    <>
+      <Button variant="primary" onClick={openRegisterModal}>
+        Register
+      </Button>
+      <Modal show={registerModal} onHide={closeRegisterModal}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered>
+        <Modal.Header closeButton>	
+          <div className="avatar">
+              <img src={logo.src} alt="logo SiMiddleman+"/>
+          </div>
+          <Modal.Title className="ms-auto">Register</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <Form onSubmit={handleSubmitRegister}>
+            <Form.Group className="mb-3">
+              <Form.Control
+                  type="text"
+                  placeholder="Nama"
+                  name='nama'
+                  autoFocus
+                  required
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Control
+                  type="text"
+                  placeholder="No HP"
+                  name='noHp'
+                  required
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Control
+                  type="text"
+                  placeholder="No Rekening"
+                  name='noRek'
+                  required
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Control
+                  type="email"
+                  placeholder="Email"
+                  name='email'
+                  required
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  name='password'
+                  required
+                  minLength={8}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Control
+                  type="password"
+                  placeholder="Confirm Password"
+                  name='confirmPassword'
+                  required
+                  minLength={8}
+              />
+            </Form.Group>
+            <Button variant='merah' type='submit' className='w-100'>Register</Button>
+          </Form>
+        </Modal.Body>
+      </Modal> 
+    </>
+  )
 }
