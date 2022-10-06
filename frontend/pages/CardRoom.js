3a2990beddf090be5c61b45b20539aa972915df2
juls <julyusmanurung@gmail.com>
2022-10-06 11:28:06 +0700

    fix error
    change ' to &apos;

--- frontend/pages/CardRoom.js
+++ frontend/pages/CardRoom.js
@@ -8,7 +8,7 @@ export default function CardRoom() {
         <Card.Title>Card Title</Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          bulk of the card&apos;s content.
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
