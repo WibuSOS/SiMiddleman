f719a3fcfe3a3569971d7afb2d48d2b654918844
Khalil Attalla Firdia <khalilattalla2209@gmail.com>
2022-10-07 09:19:31 +0700

    clean code

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,11 +1,11 @@
-import {Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import { signOut, signIn, useSession } from "next-auth/react";
 import CreateRoom from './CreateRoom';
 import RegisterForm from './register';
 import jwt from "jsonwebtoken";
 import CardRoom from './CardRoom';
 
-function Home() {    
+function Home() {
   const { data: session } = useSession();
 
   if (session) {
@@ -16,18 +16,18 @@ function Home() {
         <div className='pt-5'>
           <h2>Halo Selamat Datang, berikut merupakan data anda:</h2>
           <ul>
-            <li>Id: { decoded.ID }</li>
-            <li>Email: { decoded.Email }</li>
-            <li>Role: { decoded.Role }</li>
+            <li>Id: {decoded.ID}</li>
+            <li>Email: {decoded.Email}</li>
+            <li>Role: {decoded.Role}</li>
           </ul>
           <div className='d-flex justify-content-left'>
             <Button onClick={() => signOut()} className="mx-3">Sign out</Button>
-            <CreateRoom idPenjual={decoded.ID} sessionToken={token}/>
+            <CreateRoom idPenjual={decoded.ID} sessionToken={token} />
           </div>
         </div>
         <div className='pt-5'>
           <h2>Berikut merupakan room yang telah anda buat</h2>
-          <CardRoom/>
+          <CardRoom />
         </div>
       </div>
     )
@@ -35,10 +35,10 @@ function Home() {
   else {
     return (
       <div className='container mx-10 my-7'>
-          <Button variant="primary" onClick={() => signIn()}>
-              Login
-          </Button>
-          <RegisterForm/>
+        <Button variant="primary" onClick={() => signIn()}>
+          Login
+        </Button>
+        <RegisterForm />
       </div>
     );
   }
