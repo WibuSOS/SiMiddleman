54e49c0ac874dc568d5f38358b65c713f08c8647
Khalil Attalla Firdia <khalilattalla2209@gmail.com>
2022-10-10 13:35:45 +0700

    change table Rooms to previous one

--- backend/models/rooms.go
+++ backend/models/rooms.go
@@ -6,12 +6,10 @@ import (
 
 type Rooms struct {
 	gorm.Model
-	PenjualID     uint          `json:"penjualID,omitempty"`
-	PembeliID     *uint         `json:"pembeliID,omitempty"`
-	RoomCode      string        `json:"roomCode,omitempty" gorm:"not null;unique;type:varchar(15)"`
-	Status        string        `json:"status,omitempty" gorm:"varchar(20);default:mulai transaksi"`
-	NamaEkspedisi string        `json:"namaEkspedisi,omitempty"`
-	NomorResi     string        `json:"nomorResi,omitempty"`
-	Product       *Products     `json:"product,omitempty" gorm:"foreignKey:RoomsID"`
-	Transaction   *Transactions `json:"transaction,omitempty" gorm:"foreignKey:RoomsID"`
+	PenjualID   uint          `json:"penjualID,omitempty"`
+	PembeliID   *uint         `json:"pembeliID,omitempty"`
+	RoomCode    string        `json:"roomCode,omitempty" gorm:"not null;unique;type:varchar(15)"`
+	Status      string        `json:"status,omitempty" gorm:"varchar(20);default:mulai transaksi"`
+	Product     *Products     `json:"product,omitempty" gorm:"foreignKey:RoomsID"`
+	Transaction *Transactions `json:"transaction,omitempty" gorm:"foreignKey:RoomsID"`
 }
