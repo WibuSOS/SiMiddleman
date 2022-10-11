e6ad551a04219e29e07be5fcf65b08004011bc5f
WibuSOS <32050596+WibuSOS@users.noreply.github.com>
2022-10-11 12:14:19 +0700

    [PENDING] test Payment.js

--- frontend/tests/Payment.test.js
+++ frontend/tests/Payment.test.js
@@ -2,26 +2,55 @@ import PaymentForm from "../pages/Payment";
 import "@testing-library/jest-dom";
 import { fireEvent, render, screen } from "@testing-library/react";
 
-jest.mock('next/router', () => ({
-  useRouter() {
-    return ({
-      route: '/',
-      pathname: '',
-      query: '',
-      asPath: '',
-      push: jest.fn(),
-      events: {
-        on: jest.fn(),
-        off: jest.fn()
-      },
-      beforePopState: jest.fn(() => null),
-      prefetch: jest.fn(() => null)
-    });
-  },
-}));
+// jest.mock('next/router', () => ({
+//   useRouter() {
+//     return ({
+//       pathname: '',
+//       query: {},
+//       asPath: '',
+//       push: jest.fn(),
+//       events: {
+//         on: jest.fn(),
+//         off: jest.fn()
+//       },
+//       beforePopState: jest.fn(() => null),
+//       prefetch: jest.fn(() => null)
+//     });
+//   },
+// }));
 
-describe("Payment", () => {
-  it("Check Payment.js", () => {
+describe("when clicking the beli button", () => {
+  // let component: ReactWrapper;
+
+  // beforeEach(async () => {
+  //   const useRouter = jest.spyOn(require("next/router"), "useRouter");
+
+  //   useRouter.mockImplementation(() => ({
+  //     pathname: '/Payment',
+  //     query: {
+  //       idRoom: `${1}`,
+  //     },
+  //     asPath: '/Payment',
+  //     push: jest.fn(),
+  //     events: {
+  //       on: jest.fn(),
+  //       off: jest.fn()
+  //     },
+  //     beforePopState: jest.fn(() => null),
+  //     prefetch: jest.fn(() => null)
+  //   }));
+
+  //   await waitFor(() => {
+  //     component = mount(
+  //       <Provider store={store}>
+  //         <FavoritesPage />
+  //       </Provider>
+  //     );
+  //   });
+  //   component.find('.price-drop-nav-item').simulate('click');
+  // });
+
+  it("check Payment.js", () => {
     render(<PaymentForm />);
     // check if all components are rendered
     expect(screen.getByTestId("back_button")).toBeInTheDocument();
