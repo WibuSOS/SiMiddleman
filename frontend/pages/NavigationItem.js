9e1c96b35eb36773195d75fe9e240b6b793c0e29
juls <julyusmanurung@gmail.com>
2022-10-11 08:54:05 +0700

    fix vercel deployment error
    add conditional ternary operator in `<Link>` component with its `href`. Error has occurred in next JS because value that passsing to `href` in in `<Link>` ends up being null or undefined.

--- frontend/pages/NavigationItem.js
+++ frontend/pages/NavigationItem.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const NavItem = ({ text, href, active }) => {
   return (
-    <Link href={ href }>
+    <Link href={ href ?? ''}>
       <a className={`nav__link ${ active ? "active" : ""}`}>
         {text}
       </a>
