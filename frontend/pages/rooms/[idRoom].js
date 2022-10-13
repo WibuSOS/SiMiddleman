95a39960eaffad95dc2ccf407e4bf7576b3b2ae1
TheaKevin <theakevin01@gmail.com>
2022-10-13 12:07:20 +0700

    [REFACTORING] restructure halaman room chat

--- frontend/pages/rooms/[idRoom].js
+++ frontend/pages/rooms/[idRoom].js
@@ -5,6 +5,7 @@ import { getSession } from 'next-auth/react';
 import jwt from "jsonwebtoken";
 import ShowRoomCode from '../ShowRoomCode';
 import Swal from 'sweetalert2';
+import DetailProduk from './detailProduk';
 
 export default function Room({ user }) {
   const [data, setData] = useState(null);
@@ -82,45 +83,7 @@ export default function Room({ user }) {
     <div className='content container pt-5'>
       <Button type='submit' className='me-3'>Close</Button>
       <ShowRoomCode roomCode={data?.data.roomCode} />
-      <div className="d-flex justify-content-between">
-        <div className='pt-5'>
-          <h2>Detail Produk</h2>
-          {error && <div>Failed to load {error.toString()}</div>}
-          {!data ? <div>Loading...</div> : ((data?.data ?? []).length === 0 && <p className='text-xl p-8 text-center text-gray-100'>Data Kosong</p>)}
-          <p>{data?.data.product.deskripsi}</p>
-        </div>
-        <div className='pt-5'>
-          {data?.data.pembeliID === decoded.ID && data?.statuses.slice(0, -1).includes(data.data.status) && <Button className='me-3' onClick={() => { router.push({ pathname: '/rooms/payment/[idRoom]', query: { idRoom: `${data?.data.ID}` } }, '/rooms/payment/[idRoom]') }}>Beli</Button>}
-          {data?.data.penjualID === decoded.ID && data?.statuses.slice(1, -1).includes(data.data.status) && <Button className='me-3' onClick={() => kirimBarang()}>Kirim Barang</Button>}
-          {data?.data.pembeliID === decoded.ID && data?.statuses.slice(2, -1).includes(data.data.status) && <Button className='me-3' onClick={() => handleConfirmation()}>Barang Telah Sampai</Button>}
-        </div>
-      </div>
-      <div className='pt-5'>
-        <h5> NAMA PRODUK : </h5>
-        <p>{data?.data.product.nama}</p>
-      </div>
-      <div className="d-flex justify-content-between pt-5">
-        <div className="col">
-          <div className="row">
-            <div className="col">
-              <h5> KUANTITAS PRODUK : </h5>
-              <p> {data?.data.product.kuantitas} </p>
-            </div>
-            <div className="col">
-              <h5> HARGA PRODUK : </h5>
-              <p> {data?.data.product.harga} </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className='pt-5'>
-        <h5> STATUS TRANSAKSI : </h5>
-        <p>{data?.data.status}</p>
-      </div>
-      <div className='pt-5'>
-        <h5> DESKRIPSI PRODUK : </h5>
-        <p>{data?.data.product.deskripsi}</p>
-      </div>
+      <DetailProduk data={data} error={error} decoded={decoded} router={router} kirimBarang={kirimBarang} handleConfirmation={handleConfirmation} />
       <div className="row pt-5">
         <Button type='submit'>Checkout</Button>
       </div>
