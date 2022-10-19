7d93208e7e6c893922bdbeaebdaf2960865686da
Khalil Attalla Firdia <khalilattalla2209@gmail.com>
2022-10-19 16:28:31 +0700

    [REFACTOR] Update Testing product with localization

--- backend/controllers/product/service.go
+++ backend/controllers/product/service.go
@@ -7,11 +7,7 @@ import (
 )
 
 type Service interface {
-	// GetSpesifikProduct(idroom uint, req DataRequest) (models.Products, error)
-	// CreateProduct(idroom uint, req DataRequest) (models.Products, error)
-	// CreateProductReturnID(idroom uint, req DataRequest) (uint, error)
 	UpdateProduct(id string, req DataRequest) (models.Products, *errors.RestError)
-	// DeleteProduct(id string) *errors.RestError
 }
 
 type service struct {
@@ -21,35 +17,6 @@ type service struct {
 func NewService(repo Repository) *service {
 	return &service{repo}
 }
-
-// func (s *service) GetSpesifikProduct(idroom uint, req DataRequest) (models.Products, error) {
-// 	product, err := s.repo.GetSpesifikProduct(idroom)
-// 	if err != nil {
-// 		return models.Products{}, err
-// 	}
-
-// 	return product, nil
-// }
-
-// func (s *service) CreateProduct(idroom uint, req DataRequest) (models.Products, error) {
-
-// 	product, err := s.repo.CreateProduct(idroom, req)
-// 	if err != nil {
-// 		return models.Products{}, err
-// 	}
-
-// 	return product, nil
-// }
-
-// func (s *service) CreateProductReturnID(idroom uint, req DataRequest) (uint, error) {
-
-// 	idproduct, err := s.repo.CreateProductReturnID(idroom, req)
-// 	if err != nil {
-// 		return 0, err
-// 	}
-
-// 	return idproduct, nil
-// }
 
 func (s *service) UpdateProduct(id string, req DataRequest) (models.Products, *errors.RestError) {
 	if err := req.ValidateReq(); err != nil {
@@ -62,12 +29,3 @@ func (s *service) UpdateProduct(id string, req DataRequest) (models.Products, *e
 
 	return product, nil
 }
-
-// func (s *service) DeleteProduct(id string) *errors.RestError {
-// 	err := s.repo.DeleteProduct(id)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
