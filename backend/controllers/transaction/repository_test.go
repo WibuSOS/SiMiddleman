ea07192fd39314ae0d108841c2823aaac27d122d
Khalil Attalla Firdia <khalilattalla2209@gmail.com>
2022-10-19 18:39:00 +0700

    [REFACTOR] add param lang in handler transaction

--- backend/controllers/transaction/repository_test.go
+++ backend/controllers/transaction/repository_test.go
@@ -97,7 +97,7 @@ func TestGetDetailsPaymentErrorFetchingData(t *testing.T) {
 
 	_, err := repo.GetPaymentDetails(0)
 	assert.NotNil(t, err)
-	assert.Equal(t, "Error while fetching data", err.Message)
+	assert.Equal(t, "errorfetchingdata", err.Message)
 	assert.Equal(t, http.StatusInternalServerError, err.Status)
 	assert.Equal(t, "Internal_Server_Error", err.Error)
 }
@@ -110,7 +110,7 @@ func TestGetDetailsRoomNotFound(t *testing.T) {
 
 	_, err := repo.GetPaymentDetails(idRoom)
 	assert.NotNil(t, err)
-	assert.Equal(t, "Room not found", err.Message)
+	assert.Equal(t, "roomnotfound", err.Message)
 	assert.Equal(t, http.StatusBadRequest, err.Status)
 	assert.Equal(t, "Bad_Request", err.Error)
 }
