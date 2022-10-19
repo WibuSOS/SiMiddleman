14f45664e5ca6e2a6fd95216aa653f59e3110b37
juls <julyusmanurung@gmail.com>
2022-10-19 16:40:58 +0700

    [RED] trying to use `toLocaleString` again to test if it 's working or not
    please working properly

--- frontend/pages/CardRoom.js
+++ frontend/pages/CardRoom.js
@@ -30,8 +30,8 @@ export default function CardRoom(props) {
             <p className='card-deskripsi-produk'>{props.deskripsiProduk}</p>
           </div>
           <div>
-            {/* <p className='card-harga-produk'>Rp {props.hargaProduk.toLocaleString()}</p> */}
-            <p className='card-harga-produk'>Rp {props.hargaProduk}</p>
+            <p className='card-harga-produk'>Rp {props.hargaProduk.toLocaleString()}</p>
+            {/* <p className='card-harga-produk'>Rp {props.hargaProduk}</p> */}
           </div>
         </Card.Body>
         <Button variant="simiddleman" className='w-100' onClick={() => {router.push({ pathname: '/rooms/[idRoom]', query: {id: `${props.idRoom}`, idRoom: `${props.kodeRuangan}`,}})}}>{t("masukButton")}</Button>
