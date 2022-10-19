4a261313430698a029639909702e306f266d2179
juls <julyusmanurung@gmail.com>
2022-10-19 09:58:27 +0700

    [REFACTOR] fix login page view

--- frontend/pages/Footer.js
+++ frontend/pages/Footer.js
@@ -9,10 +9,10 @@ const Footer = () => {
       <footer>
         <div className="container">
           <div className="footer-content row d-flex justify-content-between">
-            <div className="col-lg-4 col-md-6 col-sm-6 text-white text-center"> 
+            <div className="col-lg-12 col-md-12 col-sm-12 text-white text-center"> 
               &#169; 2022, {t("footer.copyright")}
             </div>
-            <div className="col-lg-4 col-md-6 col-sm-6 text-white" hidden>
+            {/* <div className="col-lg-4 col-md-6 col-sm-6 text-white" hidden>
               <div className="row d-flex justify-content-between link">
                 <div className="col-sm-6 text-center">
                   <Link href="/">{t("footer.privacyPolicy")}</Link>
@@ -21,7 +21,7 @@ const Footer = () => {
                   <Link href="/">{t("footer.termsCondition")}</Link>
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </footer>
