c643c9b4eb8bab2c3bc3847032b9df4e43567881
Khalil Attalla Firdia <khalilattalla2209@gmail.com>
2022-10-20 08:53:56 +0700

    [REFACTOR] update Test Auth With Localization

--- backend/controllers/auth/handler_test.go
+++ backend/controllers/auth/handler_test.go
@@ -4,27 +4,77 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"strings"
 	"testing"
 
 	//"github.com/WibuSOS/sinarmas/models"
 	//"github.com/WibuSOS/sinarmas/utils/errors"
 
+	"github.com/WibuSOS/sinarmas/backend/middlewares/localizator"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
 )
 
-func TestLoginHandler(t *testing.T) {
-	db := newTestDB(t)
-	repo := NewRepository(db)
-	service := NewService(repo)
-	handler := NewHandler(service)
+type response struct {
+	Message string       `json:"message"`
+	Data    DataResponse `json:"data"`
+	Token   string       `json:"token"`
+}
 
+func setEndPointHandler(t *testing.T, db *gorm.DB) *Handler {
+	repo := NewRepository(db)
+	assert.NotNil(t, repo)
+	service := NewService(repo)
+	assert.NotNil(t, service)
+	handler := NewHandler(service)
+	assert.NotNil(t, handler)
+
+	return handler
+}
+
+func setLocalizationHandler(t *testing.T) *localizator.Handler {
+	handler, err := localizator.NewHandler()
+	assert.NotNil(t, handler)
+	assert.NoError(t, err)
+
+	return handler
+}
+
+func setRoutes(localizationHandler *localizator.Handler, endPointHandler *Handler) *gin.Engine {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
-	r.POST("/login", handler.Login)
-	payload := `{"email": "fikri@gmail.com", "password": "fikri123"}`
-	req, err := http.NewRequest("POST", "/login", strings.NewReader(payload))
+	r.Use(localizationHandler.PassLocalizator)
+	r.POST("/:lang/login", endPointHandler.Login)
+
+	return r
+}
+
+func setEnv() {
+	os.Setenv("ENVIRONMENT", "TEST")
+	os.Setenv("LOCALIZATOR_PATH", "/middlewares/localizator")
+}
+
+func TestLoginHandler(t *testing.T) {
+	setEnv()
+
+	// DB INITIALIZATION
+	db := newTestDB(t)
+
+	// LOCALIZATION HANDLER
+	localizationHandler := setLocalizationHandler(t)
+
+	// END-POINT HANDLER
+	endPointHandler := setEndPointHandler(t, db)
+
+	// ROUTES INITIALIZATION
+	r := setRoutes(localizationHandler, endPointHandler)
+
+	var res response
+
+	payload := `{"email": "penjual@custom.com", "password": "12345678"}`
+	req, err := http.NewRequest("POST", "/en/login", strings.NewReader(payload))
 	assert.NoError(t, err)
 	assert.NotNil(t, req)
 
@@ -33,13 +83,6 @@ func TestLoginHandler(t *testing.T) {
 
 	assert.Equal(t, http.StatusOK, w.Code)
 
-	type response struct {
-		Message string       `json:"message"`
-		Data    DataResponse `json:"data"`
-		Token   string       `json:"token"`
-	}
-	var res response
-
 	assert.NoError(t, json.Unmarshal(w.Body.Bytes(), &res))
 	assert.Equal(t, "success", res.Message)
 	assert.NotEqual(t, "", res.Data.Email)
@@ -47,16 +90,22 @@ func TestLoginHandler(t *testing.T) {
 }
 
 func TestLoginHandlerInvalidJSON(t *testing.T) {
-	db := newTestDB(t)
-	repo := NewRepository(db)
-	service := NewService(repo)
-	handler := NewHandler(service)
+	setEnv()
 
-	gin.SetMode(gin.ReleaseMode)
-	r := gin.Default()
-	r.POST("/login", handler.Login)
+	// DB INITIALIZATION
+	db := newTestDB(t)
+
+	// LOCALIZATION HANDLER
+	localizationHandler := setLocalizationHandler(t)
+
+	// END-POINT HANDLER
+	endPointHandler := setEndPointHandler(t, db)
+
+	// ROUTES INITIALIZATION
+	r := setRoutes(localizationHandler, endPointHandler)
+
 	payload := `{"email": "fikri@gmail.com", "password": 123}`
-	req, err := http.NewRequest("POST", "/login", strings.NewReader(payload))
+	req, err := http.NewRequest("POST", "/en/login", strings.NewReader(payload))
 	assert.NoError(t, err)
 	assert.NotNil(t, req)
 
@@ -67,16 +116,22 @@ func TestLoginHandlerInvalidJSON(t *testing.T) {
 }
 
 func TestLoginHandlerInvalidRequest(t *testing.T) {
-	db := newTestDB(t)
-	repo := NewRepository(db)
-	service := NewService(repo)
-	handler := NewHandler(service)
+	setEnv()
 
-	gin.SetMode(gin.ReleaseMode)
-	r := gin.Default()
-	r.POST("/login", handler.Login)
+	// DB INITIALIZATION
+	db := newTestDB(t)
+
+	// LOCALIZATION HANDLER
+	localizationHandler := setLocalizationHandler(t)
+
+	// END-POINT HANDLER
+	endPointHandler := setEndPointHandler(t, db)
+
+	// ROUTES INITIALIZATION
+	r := setRoutes(localizationHandler, endPointHandler)
+
 	payload := `{"name": "fikri@gmail.com", "password": "fikri123"}`
-	req, err := http.NewRequest("POST", "/login", strings.NewReader(payload))
+	req, err := http.NewRequest("POST", "/en/login", strings.NewReader(payload))
 	assert.NoError(t, err)
 	assert.NotNil(t, req)
 
