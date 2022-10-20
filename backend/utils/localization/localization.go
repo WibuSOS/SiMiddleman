7459762a44748aa12ffb64d7b9b6a7fc27bde848
WibuSOS <32050596+WibuSOS@users.noreply.github.com>
2022-10-21 06:31:20 +0700

    [FIX] test different path in collectData & GetRootPath

--- backend/utils/localization/localization.go
+++ backend/utils/localization/localization.go
@@ -59,7 +59,7 @@ func collectData() data {
 
 	rootPath := helpers.GetRootPath()
 
-	middlewaresPath := rootPath + "/middlewares"
+	middlewaresPath := rootPath + ""
 	dirMiddleware := readDirectory(middlewaresPath)
 	for _, v := range dirMiddleware {
 		if v.IsDir() && v.Name() != "localizator" {
