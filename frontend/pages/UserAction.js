104cadca046f235a59ea0a956e9cf20d7489fac9
TheaKevin <theakevin01@gmail.com>
2022-10-20 10:54:58 +0700

    [REFACTOR] restructure halaman home

--- frontend/pages/UserAction.js
+++ frontend/pages/UserAction.js
@@ -2,12 +2,9 @@ import Card from 'react-bootstrap/Card';
 import CreateRoom from './CreateRoom';
 import JoinRoom from './JoinRoom';
 import useTranslation from 'next-translate/useTranslation';
-import { useRouter } from "next/router";
-import { Button } from 'react-bootstrap';
 
 export default function UserAction ( props ) {
   const { t } = useTranslation('common');
-  const router = useRouter();
   
   return (
     <div className='user-action-wrapper'>
@@ -24,13 +21,6 @@ export default function UserAction ( props ) {
             <Card.Title className='mb-5'>{t('logged-in.user-action.title.1')}</Card.Title>
             <Card.Text>{t('logged-in.user-action.text.1')}</Card.Text>
             <JoinRoom idPembeli={props.decoded?.ID} sessionToken={props.user} />
-          </Card.Body>
-        </Card>
-        <Card className='user-action col-lg-4 col-md-5 col-sm-12'>
-          <Card.Body className='d-flex flex-column justify-content-around'>
-            <Card.Title className='mb-5'>{t('logged-in.user-action.title.2')}</Card.Title>
-            <Card.Text>{t('logged-in.user-action.text.2')}</Card.Text>
-            <Button className='w-100 btn-simiddleman' onClick={() => router.push({ pathname: '/profiles/[idUser]', query: {idUser: props.decoded?.ID}})}>{t('buttonProfile')}</Button>
           </Card.Body>
         </Card>
       </div>
